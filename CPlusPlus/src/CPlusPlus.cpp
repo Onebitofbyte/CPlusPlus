31693c302ad8bbe6536dfeee5245cd3eaa258685
Onebitofbyte <52221317+Onebitofbyte@users.noreply.github.com>
2020-05-04 18:28:40 +0100

    Forgotten Changes

--- CPlusPlus/src/CPlusPlus.cpp
+++ CPlusPlus/src/CPlusPlus.cpp
@@ -6,10 +6,44 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+
+
+// C++ Program to print
+// an Array using Recursion
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+// Recursive function to print the array
+void print_array(int arr[], int size)
+{
+
+    // using the static variable
+    static int i;
+
+    // base case
+    if (i == size) {
+        //i = 0;
+        cout << endl;
+        return;
+    }
+
+    // print the ith element
+    cout << arr[i] << " \n";
+    i++;
+
+    // recursive call
+    print_array(arr, size);
+}
+
+// Driver code
+int main()
+{
+
+    int arr[] = { 3, 5, 6, 8, 4  };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    print_array(arr, n);
+
+    return 0;
 }
